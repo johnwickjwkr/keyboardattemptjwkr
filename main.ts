3a0a228e6600ff723490cfb5c2211ab4187648ed
johnwickjwkr <74412633+johnwickjwkr@users.noreply.github.com>
2020-11-13 12:47:55 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+	
+})
